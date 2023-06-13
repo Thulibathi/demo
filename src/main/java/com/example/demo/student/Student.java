1d56669c2a006b481fcda0cc7e76875630ee9b14
Thulibathi <nokuthulamargaret@gmail.com>
2023-06-13 16:40:34 +0200

    changes

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table
@@ -19,29 +20,26 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
+    @Transient
     private Integer age;
 
     public Student(){
     }
 
     public Student(Long id, String name,
-                   String email, LocalDate dob,
-                   Integer age) {
+                   String email, LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Student(String name,
                    String email,
-                   LocalDate dob,
-                   Integer age) {
+                   LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -77,7 +75,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(dob,LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,23 +1,72 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class StudentService {
-    @GetMapping
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
     public List<Student> getStudents()
     {
-        return List.of(new Student(
-                1L,
-                "Mariam",
-                "mariam.jamal@gmail.com",
-                LocalDate.of(2000, Month.JANUARY,5),
-                21));
+        return studentRepository.findAll();
+    }
+
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository
+                .findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists){
+            throw new IllegalStateException("student with id "+studentId+" does not exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId,
+                              String name,
+                              String email){
+        Student student = studentRepository.findById(studentId).orElseThrow(()-> new IllegalStateException("student with id "+ studentId+" does not exixst"));
+
+        if(name!=null &&
+    name.length()>0 &&
+    !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+
+        if(email!=null &&
+                email.length()>0 &&
+                !Objects.equals(student.getEmail(),email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if(studentOptional.isPresent())
+            {
+                throw new IllegalStateException(("email taken"));
+            }
+            student.setEmail(email);
+        }
     }
 }
